3010acf3faca784ec7325fed19457d6766700dd8
keyur <keyur@softprodigy.com>
2019-07-04 10:23:37 +0530

    trello bug fix

--- client/collections/trendingtags.js
+++ client/collections/trendingtags.js
@@ -10,15 +10,21 @@ TrendingTags.loadTopTags = function(limit, cb) {
     'nsfw'
     // we can add more if needed
   ]
-  
+
   dateTo = moment().format('YYYY-MM-DD');
   dateFrom = moment().subtract(30,'d').format('YYYY-MM-DD');
   timeQ = 'created:>='+dateFrom
+
+
   AskSteem.trending({q: 'meta.video.info.title:* AND '+timeQ, types: 'tags', size: limit}, function(err, res) {
-    if (err) console.log(err)
+
+    if (err){
+      console.log(err)
+    }
     var results = res.results
     for (var i = 0; i < results.length; i++)
       if (excludedTags.indexOf(results[i].term) == -1)
         TrendingTags.upsert(results[i], results[i])
   })
+
 }
